d8dbf66029e894e17909befa1d828a63e1fb819e
zokirjontoshpulatov <85741100+zokirjontoshpulatov@users.noreply.github.com>
2021-07-18 23:10:07 +0500

    Add files via upload

---
+++ 6_lesson_homework/app.js
@@ -0,0 +1,13 @@
+console.log('wjsa')
+alert('O`yinimizga xush kelibsiz!!!');
+confirm("O`ynashga rozimisiz?");
+
+
+function myFunction() {
+    document.getElementById("baraban").innerHTML = "YOU CLICKED ME!";
+}           
+        
+
+
+
+
